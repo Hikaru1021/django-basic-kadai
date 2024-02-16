f1429842e5a169a59c6f6ba76de72ecab9ab5a87
Hikaru1021 <kagayaki.kirari.1021@gmail.com>
2024-02-16 10:28:23 +0900

    商品一覧の作成

--- kadai_010/crud/views.py
+++ kadai_010/crud/views.py
@@ -3,7 +3,8 @@ from django.views.generic import TemplateView, ListView
 from .models import Product
 
 class TopView(TemplateView):
-     template_name = "top.html"
+    template_name = "top.html"
 
 class ProductListView(ListView):
-     model = Product
+    model = Product
+paginate_by = 3
