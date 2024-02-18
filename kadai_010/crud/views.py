1258e4425c13b28ccce050652351112da23ca8a7
Hikaru1021 <kagayaki.kirari.1021@gmail.com>
2024-02-18 15:01:16 +0900

    詳細ページを追加

--- kadai_010/crud/views.py
+++ kadai_010/crud/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView,DeleteView
 from .models import Product
 from django.urls import reverse_lazy
@@ -9,6 +9,11 @@ class TopView(TemplateView):
 
 class ProductListView(ListView):
     model = Product
+
+class ProductDetailView(DetailView):
+    model = Product
+    temlpate_name = "crud/product_detail.html"
+
 
 class ProductCreateView(CreateView):
      model = Product
