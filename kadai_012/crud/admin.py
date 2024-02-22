9f1c5a2ac63882852421a6880ec3406a96157515
Hikaru1021 <kagayaki.kirari.1021@gmail.com>
2024-02-23 07:30:13 +0900

    商品詳細の説明途中

--- kadai_012/crud/admin.py
+++ kadai_012/crud/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category,Explanation
 from django.utils.safestring import mark_safe
 
 class ProductAdmin(admin.ModelAdmin):
-     list_display = ('id', 'name', 'price', 'category', 'image')
+     list_display = ('id', 'name', 'price', 'category', 'explanation' ,'image')
      search_fields = ('name',)
      list_filter = ('category',)
 
@@ -14,6 +14,12 @@ class CategoryAdmin(admin.ModelAdmin):
      list_display = ('id', 'name')
      search_fields = ('name',)
 
+class ExplanationAdmin(admin.ModelAdmin):
+     list_display = ('id', 'name')
+     search_fields = ('name',)
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Explanation, ExplanationAdmin)
+
 
