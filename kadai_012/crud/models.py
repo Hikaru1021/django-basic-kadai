0ce5fb493c5716ff6f1ce6cf3a09efcb85fe534a
Hikaru1021 <kagayaki.kirari.1021@gmail.com>
2024-02-25 15:59:21 +0900

    商品詳細の説明を追加済み

--- kadai_012/crud/models.py
+++ kadai_012/crud/models.py
@@ -7,19 +7,13 @@ class Category(models.Model):
      def __str__(self):
          return self.name
 
-class Explanation(models.Model):
-     name = models.TextField(blank=True, null=True)
- 
-     def __str__(self):
-         return self.name
-
 
 class Product(models.Model):
      name = models.CharField(max_length=200)
      price = models.PositiveIntegerField()
      category = models.ForeignKey(Category, on_delete=models.CASCADE)
      img = models.ImageField(blank=True, default='noImage.png')
-     
+     explanation = models.TextField(blank=True, null=True)
      def __str__(self):
          return self.name
      
